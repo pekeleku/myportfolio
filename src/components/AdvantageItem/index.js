d72fe37beb1476142b207b995610811f94c99468
pekeleku <alumanwalter@gmail.com>
2023-08-12 15:35:31 +0800

    change img => advantage

--- src/components/AdvantageItem/index.js
+++ src/components/AdvantageItem/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function index({ pathImg, title }) {
   return (
-    <div className=" w-[165px] h-[159px] mb-4 md:mb-0 p-[50px] bg-secondary   hover:bg-slate-400 hover:duration-500  rounded-[35px] mr-0 md:mr-7">
+    <div className=" w-[165px] h-[159px] mb-4 md:mb-0 p-[40px] bg-secondary   hover:bg-slate-400 hover:duration-500  rounded-[35px] mr-0 md:mr-7">
       <div className="flex  items-center justify-center ">
         <img src={pathImg} className="  " alt={title} />
       </div>
--- src/parts/Advantage.js
+++ src/parts/Advantage.js
@@ -1,20 +1,21 @@
 import React from "react";
 import AdvantageItem from "../components/AdvantageItem";
+
 export default function Advantage() {
   return (
     <section id="advantage" className="container mt-[100px] font-poppins">
       <p className="text-black font-bold text-xl">My Super Power</p>
       <div className="flex justify-center items-center mt-[55px] flex-col md:flex-row">
-        <AdvantageItem title="Figma" pathImg="./img/advantage/figma.png" />
-        <AdvantageItem title="ReactJs" pathImg="./img/advantage/react.png" />
+        <AdvantageItem title="Figma" pathImg="./img/advantage/figma.svg" />
+        <AdvantageItem title="ReactJs" pathImg="./img/advantage/react.svg" />
         <AdvantageItem
           title="TailwindCss"
-          pathImg="./img/advantage/tailwind.png"
+          pathImg="./img/advantage/tailwind.svg"
         />
-        <AdvantageItem title="Sass" pathImg="./img/advantage/sass.png" />
+        <AdvantageItem title="Sass" pathImg="./img/advantage/sass.svg" />
         <AdvantageItem
           title="Bootstrap"
-          pathImg="./img/advantage/bootstrap.png"
+          pathImg="./img/advantage/bootstrap.svg"
         />
       </div>
     </section>
